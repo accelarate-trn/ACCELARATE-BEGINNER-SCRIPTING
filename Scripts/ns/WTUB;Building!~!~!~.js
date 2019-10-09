1fb1809625bf58f2d5e79bc528578b7e0915b946
accelarate-trn <55813457+accelarate-trn@users.noreply.github.com>
2019-10-09 16:43:55 -0400

    Add files via upload

--- Scripts/ns/WTUB;Building!~!~!~.js
+++ Scripts/ns/WTUB;Building!~!~!~.js
@@ -1,6 +1,6 @@
 if (wfTask == "Permit Issuance" && wfStatus == "Issued" && balanceDue > 0)
 	{
-		showDebug = true;
+		showMessage = true;
 		comment("Pay the amount due for the record");
 		cancel = true;
 	}
