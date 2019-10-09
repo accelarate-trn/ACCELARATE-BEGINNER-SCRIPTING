6d8cbe43bd622b76e53bf40b61ae84a7ac09228b
accelarate-trn <55813457+accelarate-trn@users.noreply.github.com>
2019-10-09 16:31:27 -0400

    Add files via upload

---
+++ Scripts/mmg/WTUB;Building!~!~!~.js
@@ -0,0 +1,5 @@
+If (wfTask == "Permit Issuance" && wfStatus == "Issued" && balanceDue> 0) {
+showDebug = true;
+comment("This action cannot be done with balance due on the record.");
+cancel = true;
+}
