c87605a9e4bb67693a5d67df413d5ce17a51c40d
accelarate-trn <55813457+accelarate-trn@users.noreply.github.com>
2019-10-09 16:24:22 -0400

    Delete WTUB;Building!`!`!`.js

--- Scripts/SN/WTUB;Building!`!`!`.js
+++
@@ -1,5 +0,0 @@
-if (wfTask =="Permit Issuance" && wfStatus == "Issued" && balanceDue > 0){
-	showDebug = true;
-	comment ( "If stutus is issued and no balance due then pay it Saher" );
-	cancel = true;
-}
