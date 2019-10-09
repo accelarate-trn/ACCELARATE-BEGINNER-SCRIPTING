4f633d9629b7e9be48a98c63a6672189e53b7b4f
accelarate-trn <55813457+accelarate-trn@users.noreply.github.com>
2019-10-09 14:19:40 -0600

    Update WTUB;BUILDING!~!~!~.js

--- Scripts/rh_cot/WTUB;BUILDING!~!~!~.js
+++ Scripts/rh_cot/WTUB;BUILDING!~!~!~.js
@@ -1,6 +1,5 @@
 if (wfTask == "Permit Issuance" && wfStatus == "Issued" && balanceDue > 0) {
 	showDebug = true;
-	//let balDue = balanceDue;
-	comment('This action cannot be taken. Balance remaining: $');
+	comment("This action cannot be taken due to a remaining balance.");
 	cancel = true;
 }
