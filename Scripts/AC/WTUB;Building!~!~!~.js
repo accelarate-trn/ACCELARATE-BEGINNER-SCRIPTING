35b73ada2be21e4dce23dc9862f5cd5ee559675d
accelarate-trn <55813457+accelarate-trn@users.noreply.github.com>
2019-10-09 14:38:08 -0600

    Update WTUB;Building!~!~!~.js

--- Scripts/AC/WTUB;Building!~!~!~.js
+++ Scripts/AC/WTUB;Building!~!~!~.js
@@ -3,3 +3,6 @@ if(wfTask == "Pertmit Issuance" &&  wfStatus == "Issued" && balanceDue > 0){
 	comment("There is a balance due");
 	cancel = true;
 }
+else{
+	comment("There is no balance due, continue");
+}
