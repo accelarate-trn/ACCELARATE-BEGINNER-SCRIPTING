63da1fc9329358527c8a3db91eaab816452fc41f
accelarate-trn <55813457+accelarate-trn@users.noreply.github.com>
2019-10-09 16:19:15 -0400

    Update WTUB;Building!~!~!~.js

--- Scripts/nt/WTUB;Building!~!~!~.js
+++ Scripts/nt/WTUB;Building!~!~!~.js
@@ -1,5 +1,5 @@
 if (wfTask == "Permit Issuance" && wfStatus == "Issued" && balanceDue > 0) {
- showDeg = true;
+ showDebug = true;
  comment ("This action cannot be done with balance due on the record.");
  cancel = true;
 }
