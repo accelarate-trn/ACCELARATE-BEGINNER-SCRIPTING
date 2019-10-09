03b682e5e3e5731527ad78a3ad62e115c037c467
accelarate-trn <55813457+accelarate-trn@users.noreply.github.com>
2019-10-09 14:30:54 -0600

    Delete WTUB;Building!~!~!~.js

--- Scripts/mmg/WTUB;Building!~!~!~.js
+++
@@ -1,5 +0,0 @@
-If (wfTask == "Permit Issuance" &amp;&amp; wfStatus == "Issued" &amp;&amp; balanceDue> 0) {
-showDebug = true;
-comment("This action cannot be done with balance due on the record.");
-cancel = true;
-}
