52f05dc350c96bca1f47826ab233d12dce7e18b5
accelarate-trn <55813457+accelarate-trn@users.noreply.github.com>
2019-10-09 18:28:40 -0400

    Add files via upload

--- Scripts/AC/WTUA;BUILDING!COMMERCIAL!ALTERATION!~.js
+++ Scripts/AC/WTUA;BUILDING!COMMERCIAL!ALTERATION!~.js
@@ -1,3 +1,6 @@
-if(wfTask == "Accepted - Plan Review Req"){
+if(wfTask == "Application Intake" && wfStatus == "Accepted - Plan Review Req"){
 	updateFee("BLDGCOMALT04","BLDG_COM_ALT","FINAL",1,"Y");
+	showMessage("Fee applied");
+}else{
+	showMessage("Fee not applied");
 }
