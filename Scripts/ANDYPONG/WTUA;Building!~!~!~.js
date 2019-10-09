6943764207ffbbd191f0ea68b00a0974a9f66d52
accelarate-trn <55813457+accelarate-trn@users.noreply.github.com>
2019-10-09 18:00:05 -0400

    Add files via upload

---
+++ Scripts/ANDYPONG/WTUA;Building!~!~!~.js
@@ -0,0 +1,8 @@
+
+	if(wfTask == "Application Intake" && wfStatus == "Accepted - Plan Review Req" && balanceDue > 0)
+	{
+		showMessage = true;
+		updateFee("BLDGCOMALT04", "BLDG_COM_ALT", null, 1, "Y", null)
+
+	}
+	
