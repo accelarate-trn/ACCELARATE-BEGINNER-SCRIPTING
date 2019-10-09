774801a28decdce528ba2b2cba45ac9ef2f1119c
accelarate-trn <55813457+accelarate-trn@users.noreply.github.com>
2019-10-09 18:43:33 -0400

    Add files via upload

---
+++ Scripts/dm/CONF_BUILDINGRECORDAUTOMATION.js
@@ -0,0 +1,33 @@
+{
+  "Building/Commercial/Alteration/NA": {
+    "WorkflowTaskUpdateAfter": [
+      {
+        "metadata": {
+          "description": "test records",
+          "operators": {}
+        },
+        "description": "Individual Exercise 1",
+        "criteria": {
+          "task": [
+            "Application Intake"
+          ],
+          "status": [
+            "Accepted - Plan Review Req"
+          ]
+        },
+        "action": {
+          "assessFees": [
+            {
+              "feeSchedule": "BLDG-COM-ALT",
+              "feeCode": "BLDGCOMALT04",
+              "feeQuantity": 1,
+              "feeInvoice": "Y",
+              "feePeriod": "final"
+            }
+          ]
+        },
+        "postScript": ""
+      }
+    ]
+  }
+}
