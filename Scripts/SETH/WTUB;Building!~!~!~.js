c0a993012c65489ba4001c5c5d1d414811bc3461
accelarate-trn <55813457+accelarate-trn@users.noreply.github.com>
2019-10-09 16:15:53 -0400

    Add files via upload

---
+++ Scripts/SETH/WTUB;Building!~!~!~.js
@@ -0,0 +1,16 @@
+
+
+if (wfTask == "Permit Issuance" && wfStatus == "Issued" && balanceDue > 0)
+{
+	showDebug = true;
+	comment("any comment that you want");
+	cancel = true;
+	
+	
+} 
+
+	
+
+	
+
+
